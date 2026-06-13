e39f1e37986f990b0d002d6d0696e353ce5b7648
Jackson Faria <jacksonaparesido@gmail.com>
2026-06-12 21:41:30 -0300

    feat: adiciona rota /admin sem navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import { Navbar } from './components/Navbar';
 import { HomePage } from './pages/HomePage';
@@ -6,22 +6,34 @@ import { ParticipantesPage } from './pages/ParticipantesPage';
 import { AulasPage } from './pages/AulasPage';
 import { AboutPage } from './pages/AboutPage';
 import { OfficePage } from './pages/OfficePage';
+import { AdminPage } from './pages/AdminPage';
 
 import './index.css';
+
+function Layout() {
+  const location = useLocation();
+  const isAdmin = location.pathname === '/admin';
+
+  return (
+    <>
+      {!isAdmin && <Navbar />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/participantes" element={<ParticipantesPage />} />
+        <Route path="/aulas" element={<AulasPage />} />
+        <Route path="/office" element={<OfficePage />} />
+        <Route path="/sobre" element={<AboutPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
       <AppProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/participantes" element={<ParticipantesPage />} />
-          <Route path="/aulas" element={<AulasPage />} />
-          <Route path="/office" element={<OfficePage />} />
-          <Route path="/sobre" element={<AboutPage />} />
-
-        </Routes>
+        <Layout />
       </AppProvider>
     </Router>
   );
